d8691edf9a5e0c3e1aa9dd0f1a369369cdaf1196
5pecia1 <pdpxpd@gmail.com>
2022-02-27 22:57:25 +0900

    update script

--- scripts/run_jmusicbot.sh
+++ scripts/run_jmusicbot.sh
@@ -4,15 +4,11 @@
 # startup (and download it if the latest version isn't currently downloaded)
 DOWNLOAD=true
 
-# This will cause the script to run in a loop so that the bot auto-restarts
-# when you use the shutdown command
-LOOP=true
-
 download() {
     if [ $DOWNLOAD = true ]; then
-        URL=$(curl -s https://api.github.com/repos/jagrosh/MusicBot/releases/latest \
-           | grep -i browser_download_url.*\.jar \
-           | sed 's/.*\(http.*\)"/\1/')
+        URL=$(curl -s https://api.github.com/repos/jagrosh/MusicBot/releases/latest |
+            grep -i browser_download_url.*\.jar |
+            sed 's/.*\(http.*\)"/\1/')
         FILENAME=$(echo $URL | sed 's/.*\/\([^\/]*\)/\1/')
         if [ -f $FILENAME ]; then
             echo "Latest version already downloaded (${FILENAME})"
@@ -26,10 +22,5 @@ run() {
     java -Dnogui=true -jar $(ls -t JMusicBot* | head -1)
 }
 
-while
-    download
-    run
-    $LOOP
-do
-    continue
-done 
+download
+run
