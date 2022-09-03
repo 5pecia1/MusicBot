ad754d090857e9f5841418637f2cebf93353210d
5pecia1 <pdpxpd@gmail.com>
2022-09-03 21:49:13 +0900

    downgrade version from 0.3.8 to 0.3.7

--- scripts/run_jmusicbot.sh
+++ scripts/run_jmusicbot.sh
@@ -6,8 +6,10 @@ DOWNLOAD=true
 
 download() {
     if [ $DOWNLOAD = true ]; then
-        URL=$(curl -s https://api.github.com/repos/jagrosh/MusicBot/releases/latest |
-            grep -i browser_download_url.*\.jar |
+        # URL=$(curl -s https://api.github.com/repos/jagrosh/MusicBot/releases/latest |
+        #     grep -i browser_download_url.*\.jar |
+        #     sed 's/.*\(http.*\)"/\1/')
+        URL=$(echo https://github.com/jagrosh/MusicBot/releases/download/0.3.7/JMusicBot-0.3.7.jar |
             sed 's/.*\(http.*\)"/\1/')
         FILENAME=$(echo $URL | sed 's/.*\/\([^\/]*\)/\1/')
         if [ -f $FILENAME ]; then
